855912b9069f6a8e8a756be3e3fa99500a330906
FadimeB <kafkaskizi@hotmail.com>
2023-03-07 12:42:43 -0500

    Added US_95

---
+++ src/test/java/com/CRM_Openly/step_definitions/US95_DeskTopStepDefs.java
@@ -0,0 +1,33 @@
+package com.CRM_Openly.step_definitions;
+
+import com.CRM_Openly.pages.DesktopOptionsPage;
+import com.CRM_Openly.utilities.BrowserUtils;
+import com.CRM_Openly.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+public class US95_DeskTopStepDefs {
+
+    DesktopOptionsPage desktopOptionsPage = new DesktopOptionsPage();
+
+
+    @Given("User should scroll down")
+    public void user_should_scroll_down() throws InterruptedException {
+        Thread.sleep(3000);
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("window.scrollBy(0,400)");
+
+        Thread.sleep(3000);
+
+    }
+
+
+    @Then("User should see {string}")
+    public void userShouldSee(String DesktopClient) {
+        BrowserUtils.verifyElementDisplayed(desktopOptionsPage.DesktopClient);
+    }
+}
+
---
+++ src/test/resources/features/US95_Fadime.feature
@@ -0,0 +1,8 @@
+Feature: User should be able to see desktop options to download
+
+  Background: user is already in the login page
+    Given the user logged in with username as "hr1@cybertekschool.com" and password as "UserUser"
+
+  Scenario: Verify desktop options to download
+    Given User should scroll down
+    Then User should see "desktop client"
