e711d4718fa59451fd8cd8fb2f255d305a3f64e2
Liuba-SDET <lkurbanova075@gmail.com>
2023-03-07 13:24:53 -0500

    json plugin added

--- src/test/java/com/CRM_Openly/runners/CukesRunner.java
+++ src/test/java/com/CRM_Openly/runners/CukesRunner.java
@@ -7,14 +7,15 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "json:target/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/CRM_Openly/step_definitions",
-        dryRun = true,
-        tags = "",
+        dryRun = false,
+        tags = "@B28G17-113",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
