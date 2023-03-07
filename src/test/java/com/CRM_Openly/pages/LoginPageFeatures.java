e48e269cbdee74443ad9296577d9ade2da02ff75
Liuba-SDET <lkurbanova075@gmail.com>
2023-03-07 10:27:21 -0500

    LoginPageFeatures_StepDefs updated

--- src/test/java/com/CRM_Openly/pages/LoginPageFeatures.java
+++ src/test/java/com/CRM_Openly/pages/LoginPageFeatures.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPageFeatures {
 
-    LoginPageFeatures(){
+   public LoginPageFeatures(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
